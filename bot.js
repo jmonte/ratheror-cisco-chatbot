c501fd80df6cf3df2ec4fe30a0c68fd7880de29f
Awkward Minimalist <awkwardminimalist.com@gmail.com>
2017-07-22 23:23:13 +0800

    Added conversation ang sample question

--- bot.js
+++ bot.js
@@ -1,110 +1,136 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
-
-
-This is a sample Cisco Spark bot built with Botkit.
-
-# RUN THE BOT:
-  Follow the instructions here to set up your Cisco Spark bot:
-    -> https://developer.ciscospark.com/bots.html
-  Run your bot from the command line:
-    access_token=<MY BOT ACCESS TOKEN> public_address=<MY PUBLIC HTTPS URL> node bot.js
-
-
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-var env = require('node-env-file');
-env(__dirname + '/.env');
-
-
-if (!process.env.access_token) {
-    console.log('Error: Specify a Cisco Spark access_token in environment.');
-    usage_tip();
-    process.exit(1);
-}
-
-if (!process.env.public_address) {
-    console.log('Error: Specify an SSL-enabled URL as this bot\'s public_address in environment.');
-    usage_tip();
-    process.exit(1);
-}
-
 var Botkit = require('botkit');
-var debug = require('debug')('botkit:main');
+var localtunnel = require('localtunnel');
 
-// Create the Botkit controller, which controls all instances of the bot.
-var controller = Botkit.sparkbot({
-    // debug: true,
-    // limit_to_domain: ['mycompany.com'],
-    // limit_to_org: 'my_cisco_org_id',
-    public_address: process.env.public_address,
-    ciscospark_access_token: process.env.access_token,
-    studio_token: process.env.studio_token, // get one from studio.botkit.ai to enable content management, stats, message console and more
-    secret: process.env.secret, // this is an RECOMMENDED but optional setting that enables validation of incoming webhooks
-    webhook_name: 'Cisco Spark bot created with Botkit, override me before going to production',
-    studio_command_uri: process.env.studio_command_uri,
-});
+var port = 3000;
 
-// Set up an Express-powered webserver to expose oauth and webhook endpoints
-var webserver = require(__dirname + '/components/express_webserver.js')(controller);
-
-// Tell Cisco Spark to start sending events to this application
-require(__dirname + '/components/subscribe_events.js')(controller);
-
-// Enable Dashbot.io plugin
-require(__dirname + '/components/plugin_dashbot.js')(controller);
-
-var normalizedPath = require("path").join(__dirname, "skills");
-require("fs").readdirSync(normalizedPath).forEach(function(file) {
-  require("./skills/" + file)(controller);
-});
+var tunnel = localtunnel(port, function(err, tunnel) {
+    if (err) console.log(err);
+    // the assigned public url for your tunnel
+    // i.e. https://abcdefgjhij.localtunnel.me
+    // tunnel.url;
 
 
-// This captures and evaluates any message sent to the bot as a DM
-// or sent to the bot in the form "@bot message" and passes it to
-// Botkit Studio to evaluate for trigger words and patterns.
-// If a trigger is matched, the conversation will automatically fire!
-// You can tie into the execution of the script using the functions
-// controller.studio.before, controller.studio.after and controller.studio.validate
-if (process.env.studio_token) {
-    controller.on('direct_message,direct_mention', function(bot, message) {
-        if (message.text) {
-            controller.studio.runTrigger(bot, message.text, message.user, message.channel).then(function(convo) {
-                if (!convo) {
-                    // no trigger was matched
-                    // If you want your bot to respond to every message,
-                    // define a 'fallback' script in Botkit Studio
-                    // and uncomment the line below.
-                    controller.studio.run(bot, 'fallback', message.user, message.channel);
-                } else {
-                    // set variables here that are needed for EVERY script
-                    // use controller.studio.before('script') to set variables specific to a script
-                    convo.setVar('current_time', new Date());
-                }
-            }).catch(function(err) {
-                if (err) {
-                    bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err);
-                    debug('Botkit Studio: ', err);
-                }
-            });
-        }
+
+
+
+
+    var public_address = tunnel.url;
+    var cisco_token = 'Y2NhOGI0YWUtNjY4Ny00NmIzLWI5MjktZmU5OWQzNmJlZGI0ODYyZjRmYzItNzU1';
+    var studio_token = 'QQK3Nj9y0gFwMQiDFD3AWDimrCU38CLZu8Dvs1CIOYgr38WGb6CEkSd1BZW9DfX1';
+    var secret = 'jeffrey';
+
+    var controller = Botkit.sparkbot({
+        debug: true,
+        log: true,
+        public_address: public_address,
+        ciscospark_access_token: cisco_token,
+        secret: secret,
+        json_file_store: 'course.json'
     });
-} else {
-    console.log('~~~~~~~~~~');
-    console.log('NOTE: Botkit Studio functionality has not been enabled');
-    console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/');
-}
 
-function usage_tip() {
-    console.log('~~~~~~~~~~');
-    console.log('Botkit Studio Starter Kit');
-    console.log('Execute your bot application like this:');
-    console.log('access_token=<MY ACCESS TOKEN> public_address=<https://mybotapp/> node bot.js');
-    console.log('Get Cisco Spark token here: https://developer.ciscospark.com/apps.html')
-    console.log('Get a Botkit Studio token here: https://studio.botkit.ai/')
-    console.log('~~~~~~~~~~');
-}
+
+    var bot = controller.spawn({
+    });
+
+    controller.setupWebserver(process.env.PORT || 3000, function(err, webserver) {
+        controller.createWebhookEndpoints(webserver, bot, function() {
+            console.log("SPARK: Webhooks set up!");
+        });
+    });
+
+    // participants
+    controller.hears('All Files', 'message_received,direct_message', function(bot, message) {
+       // bot.reply(message, {
+       //  attachments: [
+       //      {
+       //          contentType: 'image/png',
+       //          contentUrl: 'https://upload.wikimedia.org/wikipedia/en/a/a6/Bender_Rodriguez.png',
+       //          name: 'Bender_Rodriguez.png'
+       //      }
+       //  ]
+       //  });
+       controller.storage.users.save({id: message.user, foo:'bar'}, function(err) {  });
+       bot.reply(message,{text: 'Hello', markdown: '*Hello!*'});
+    });
+
+    controller.on('direct_mention', function(bot, message) {
+        bot.reply(message, 'You mentioned me and said, "' + message.text + '"');
+    });
+
+    // controller.hears('Last Quizes', 'direct_message,direct_mention', function(bot, message) {
+
+    // });
+
+    // controller.hears('Active Contest', 'direct_message,direct_mention', function(bot, message) {
+
+    // });
+
+    // controller.hears('Send Feedback', 'direct_message,direct_mention', function(bot, message) {
+
+    // });
+
+
+
+    // // Admin Powers
+    // controller.hears('Create Survey', 'direct_message,direct_mention', function(bot, message) {
+
+    // });
+
+    // controller.hears('Make Announcement', 'direct_message,direct_mention', function(bot, message) {
+
+    // });
+
+    // controller.hears('Question Sent', 'direct_message,direct_mention', function(bot, message) {
+
+    // });
+
+    // controller.hears('Broadcast File', 'direct_message,direct_mention', function(bot, message) {
+
+    // });
+
+
+
+    // controller.hears('hello', 'direct_message,direct_mention', function(bot, message) {
+    //     bot.reply(message, 'Hi');
+    // });
+
+    // controller.on('direct_mention', function(bot, message) {
+    //     bot.reply(message, 'You mentioned me and said, "' + message.text + '"');
+    // });
+
+    controller.on('direct_message', function(bot, message) {
+        // bot.reply(message, 'I got your private message. You said, "' + message.text + '"');
+
+            console.log('start conversation');
+              bot.startConversation(message, function(err, convo) {
+
+                convo.setVar('question_id','kh62');
+                // convo.say('Did someone say cookies!?!!');
+                convo.ask({markdown:"#1 What is your favorite color? \n1. Blue \n2. Green \n3. Red", text: 'What is your favorite color?' } , function(response, convo) {
+                    console.log( convo.vars.question_id);
+                    convo.say('Golly, I love ' + response.text + ' too!!!');
+                    switch(response.text) {
+                        case "1":
+                        case "2":
+                        case "3":
+                            controller.storage.questions.save({ id: 'sdfd', user_id: message.user, vote: response.text}, function(err) { console.log('Save'); });
+                            break;
+                        default:
+                            convo.repeat();
+                    }
+                    convo.next();
+                });
+
+                //  convo.ask({markdown:"#2 What is your favorite color? \n1. Blue \n2. Green \n3. Red", text: 'What is your favorite color?' } , function(response, convo) {
+                //     convo.say('Golly, I love ' + response.text + ' too!!!');
+                //     convo.next();
+                // });
+            });
+        
+    });
+
+});
+
+tunnel.on('close', function() {
+    // tunnels are closed
+});
